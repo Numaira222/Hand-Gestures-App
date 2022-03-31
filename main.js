00a6328c010a9f39135c35a7d2fd0247733e407e
Numaira222 <77472414+Numaira222@users.noreply.github.com>
2022-03-30 19:14:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-prediction_1 = ""
-prediction_2 = ""
+prediction_1 = "";
+prediction_2 = "";
+meaning="";
 Webcam.set({
     width: 350,
     height: 300,
@@ -28,9 +29,13 @@ function check() {
 
 function speak() {
     var synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is " + prediction_1;
-    speak_data_2 = "And the second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    //speak_data_1 = "The first prediction is " + prediction_1;
+   //speak_data_2 = "And the second prediction is " + prediction_2;
+   speak_data_3 = "This gesture means - " + meaning;
+   console.log(meaning);
+
+    
+    var utterThis = new SpeechSynthesisUtterance(speak_data_3);
     synth.speak(utterThis);
 }
 
@@ -41,38 +46,49 @@ function gotResult(error, results) {
         console.log(results);
         document.getElementById("result_emotion_name").innerHTML = results[0].label;
         document.getElementById("result_emotion_name2").innerHTML = results[1].label;
-        prediction_1 = results[0].label;
-        prediction_2 = results[1].label;
-        speak();
+        //prediction_1 = results[0].label;
+        //prediction_2 = results[1].label;
+        //speak();
         if (results[0].label == "Thumbs-up") {
+            meaning="All the best";
             document.getElementById("update_emoji").innerHTML = "&#128077;";
         }
         if (results[0].label == "Pointing-upwards") {
+            meaning="Look up";
             document.getElementById("update_emoji").innerHTML = "&#128070;";
         }
         if (results[0].label == "Peace-sign") {
+            meaning="That was a marvelous victory";
             document.getElementById("update_emoji").innerHTML = "&#9996;";
         }
         if (results[0].label == "Fist-bump") {
+            meaning="We did it";
             document.getElementById("update_emoji").innerHTML = "&#128074;";
         }
         if (results[0].label == "Thumbs-down") {
+            meaning="Not good";
             document.getElementById("update_emoji").innerHTML = "&#128078;";
         }
 
         if (results[1].label == "Thumbs-up") {
+            //meaning="All the best";
             document.getElementById("update_emoji2").innerHTML = "&#128077;";
         }
         if (results[1].label == "Pointing-upwards") {
+            ///meaning="Look up";
             document.getElementById("update_emoji2").innerHTML = "&#128070;";
         }
         if (results[1].label == "Peace-sign") {
+           // meaning="That was a marvelous victory";
             document.getElementById("update_emoji2").innerHTML = "&#9996;";
         }
         if (results[1].label == "Fist-bump") {
+            //meaning="We did it";
             document.getElementById("update_emoji2").innerHTML = "&#128074;";
         }
         if (results[1].label == "Thumbs-down") {
+            //meaning="Not good";
             document.getElementById("update_emoji2").innerHTML = "&#128078;";
         }
+        speak();
 }}
